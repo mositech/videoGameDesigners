1802fbce1ea3a14a2755fe0782aba97b6494f1b8
Chase Starr <chasestarr@gmail.com>
2015-11-04 03:02:09 -0500

    Push project 01

---
+++ 01_Platformer/Assets/Scripts/respawn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawn : MonoBehaviour {
+
+	public GameObject fireworks;
+	private int seconds;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.name == "Respawn_Zone"){
+			Debug.Log("The game reset");
+			Application.LoadLevel (0); 
+		}
+
+		if(col.gameObject.tag == "Finish"){
+			winSequence();
+		}
+	}
+
+	void winSequence(){
+		fireworks.gameObject.SetActive(true);
+		StartCoroutine(wait());
+		//Application.LoadLevel(0);
+	}
+
+
+	IEnumerator wait() {
+		//print(Time.time);
+		yield return new WaitForSeconds(5);
+		//print(Time.time);
+	}
+}
