57e01c08cdf5125a54ffdb2d674efb7d1c475e00
Chase Starr <chasestarr@gmail.com>
2015-11-04 03:03:23 -0500

    Add project 02 to repo

---
+++ 02_XboxController/Assets/scripts/shoot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class shoot : MonoBehaviour 
+{
+	public float bulletSpeed = 10;
+	public Rigidbody bullet;
+	public Camera normalCam;
+	public Camera zoomCam;
+	
+	void Fire()
+	{
+		Rigidbody bulletClone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
+		bulletClone.velocity = transform.forward * bulletSpeed;
+	}
+
+	void scope(){
+		normalCam.enabled = false;
+		zoomCam.enabled = true;
+	}
+	
+	void Update () 
+	{
+
+		Debug.Log(Input.GetAxis("xbox_Rtrigger"));
+		if (Input.GetAxis("xbox_Rtrigger") > 0.9f && !Input.GetButton("xbox_Lbumper")){
+			Fire();
+			//Debug.Log("firing");
+		}
+			
+		if(Input.GetButton("xbox_Lbumper")){
+			scope();
+			//Debug.Log("SCOPE");
+			if(Input.GetAxis("xbox_Rtrigger") > 0 && Input.GetButton("xbox_Lbumper")){
+				//`Debug.Log("Shooting");
+				Fire ();
+			}
+		}
+		else{
+			zoomCam.enabled = false;
+			normalCam.enabled = true;
+		}
+	}
+}
---
+++ 02_XboxController/Assets/scripts/testControlVal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testControlVal : MonoBehaviour {
+
+	public Transform prefab;
+	public Material blue;
+	public Material red;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(Input.GetAxis("xbox_RstickX"));
+		Debug.Log(Input.GetAxis("xbox_RstickY"));
+//		if(Input.GetButton("PS4_L2")){
+//			prefab.gameObject.renderer.material = red;
+//			Debug.Log("asf");
+//			//prefab.gameObject.renderer.material.color = Color.magenta;
+//		}
+//		else{
+//			prefab.gameObject.renderer.material = blue;
+//			//prefab.gameObject.renderer.material.color = Color.cyan;
+//		}
+	
+	}
+}
